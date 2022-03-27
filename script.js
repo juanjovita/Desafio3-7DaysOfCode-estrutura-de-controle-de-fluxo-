8b460d00007988f567c33b44459f532a2276b5d0
juanjovita <juanjovita22@gmail.com>
2022-03-27 20:52:55 -0300

    1 de 3 completo

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('hello!')
+var resposta1 = prompt('Qual área deseja se especializar?  Digite 1 para FRONT-END ou 2 para BACK-END.')
+var opcao1 = ""
+var opcao2 = ""
+
+if(resposta1 ==1){
+  opcao1 = prompt('Você escolheu FRONT-END. Qual framework deseja se especializar? Digite 1 para REACT ou 2 para VUE.')
+}
+else if(resposta1 ==2){
+opcao2 = prompt('Você escolheu BACK END! Digite 1 para JAVA ou 2 para C# .')
+}
